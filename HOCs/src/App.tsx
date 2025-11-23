04b1c5b95b0d8fbcb1f0fb131527d2fb0b7d063d
KaraniAbdellah <abdellahkarani@gmail.com>
2025-11-23 14:41:28 +0100

    feat: SetUp React With BUN and Add Two Components

--- HOCs/src/App.tsx
+++ HOCs/src/App.tsx
@@ -1,12 +1,10 @@
 import Person1 from "./Person1";
 import Person2 from "./Person2";
-
 export function App() {
   return (
     <div>
-      <h2>HOC (High Order Components)</h2>
-      <Person1></Person1>
-      <Person2></Person2>
+      <Person1 />
+      <Person2 />
     </div>
   );
 }
--- HOCs/src/Person2.tsx
+++ HOCs/src/Person2.tsx
@@ -1,12 +1,22 @@
-import EnhancedComponent from "./HOC";
+import { useState } from "react";
 
-const Person2 = ({ money, increaseMoney }) => {
+const Person2 = () => {
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <h3>Y Has: {money}</h3>
-      <button onClick={increaseMoney}>Increase it</button>
+    <div className="p-6 max-w-sm mx-auto rounded-2xl bg-green-100 shadow-sm flex items-center justify-between">
+      <h3 className="text-lg font-semibold text-green-800">
+        Sara has: <span className="font-bold">{count}</span>
+      </h3>
+
+      <button
+        onClick={() => setCount(count + 1)}
+        className="px-4 py-2 bg-green-600 text-white rounded-xl hover:bg-green-700 active:scale-95 transition"
+      >
+        Increase
+      </button>
     </div>
   );
 };
 
-export default EnhancedComponent(Person2);
+export default Person2;
